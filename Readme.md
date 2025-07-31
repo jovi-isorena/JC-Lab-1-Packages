3c874d1f7f85385199d3e87d9d60d7f8f057e724
jovito.isorena <jovito.isorena.jr@gmail.com>
2025-07-31 13:55:41 +0800

    completed lab

---
+++ Readme.md
@@ -0,0 +1,6 @@
+# Name
+Isorena, Jovito
+
+# Screenshots
+![image info](./screenshots/import-package.png)
+![image info](./screenshots/test-result.png)
---
+++ src/main/java/org/example/main/Main.java
@@ -0,0 +1,17 @@
+package org.example.main;
+
+import org.example.packageone.ClassOne;
+import org.example.packagetwo.ClassTwo;
+import org.example.packagethree.ClassThree;
+import static java.lang.System.*;
+
+// Import here manually
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        out.println(new ClassOne().greet());
+        out.println(new ClassTwo().greet());
+        out.println(new ClassThree().greet());
+    }
+}
